9839cf9fa2d07fd497d22011f278bc5a4e180de9
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2025-11-02 11:00:57 -0300

    desing 3

--- Projeto_doacoes/resources/views/Doações/index.blade.php
+++ Projeto_doacoes/resources/views/Doações/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doações</title>
+    <link rel="icon" href="favicond.svg" type="image/png">
     <style>
       body {
         font-family: 'Poppins', sans-serif;
@@ -107,7 +108,7 @@
 <body>
   <header>
     <h1 class="titulo">Doações Araucária</h1>
-    <h2 class="subtitulo">Projeto de extensão em Engenharia de Software voltado à erradicaçãoda pobreza por meio da divulgação de intituições beneficientes</h2>
+    <h2 class="subtitulo">Projeto de extensão em Engenharia de Software voltado à divulgação de intituições beneficientes</h2>
   </header>
   <main>
   <h1 class="list"><ul><li>Listagem de Intituições/Ongs</li></ul></h1>
